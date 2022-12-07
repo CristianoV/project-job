4eb6c1e3ae4efb8a5340d463f79b63974f0ebbd0
CristianoV <cristianoviieira@gmail.com>
2022-12-07 11:42:08 -0300

    feat: adiciona função que filtra todas a empresas

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,13 @@
 from typing import List, Dict
+import csv
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    with open(path) as file:
+        data = csv.DictReader(file)
+        data = [data_list for data_list in data]
+        industries = set([x["industry"] for x in data if x['industry'] != ''])
+        return list(industries)
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
