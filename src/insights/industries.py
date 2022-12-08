553040489571dfd4946a81eb346bad97df0af78b
CristianoV <cristianoviieira@gmail.com>
2022-12-08 18:12:42 -0300

    feat: adiciona função que filtra os trabalhos por industria

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -10,18 +10,6 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    data = [data_list for data_list in jobs]
+    industries = [x for x in data if x["industry"] == industry]
+    return industries
