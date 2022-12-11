93e2e1858d18b639e4e8ea53f826d70428732d18
CristianoV <cristianoviieira@gmail.com>
2022-12-11 16:23:08 -0300

    feat: adiciona teste que verifica se função traduz tabelas

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    assert (
+        read_brazilian_file("tests/mocks/brazilians_jobs.csv")[0]
+        == {'salary': '2000', 'title': 'Maquinista', 'type': 'trainee'}
+    )
+    assert (
+        read_brazilian_file("tests/mocks/brazilians_jobs.csv")[1]
+        == {'salary': '3000', 'title': 'Motorista', 'type': 'full time'}
+    )
