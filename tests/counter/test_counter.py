5d952bc44b693bf46f9609c7febd1d759e970676
CristianoV <cristianoviieira@gmail.com>
2022-12-11 16:03:20 -0300

    feat: adiciona teste para a função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "word") == 382
+    assert count_ocurrences("data/jobs.csv", "WORD") == 382
+    assert count_ocurrences("data/jobs.csv", "word") != 381
+    assert count_ocurrences("data/jobs.csv", "WORD") != 381
